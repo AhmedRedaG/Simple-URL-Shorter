4fbd700aa2f522e485886e60dba83ddbfa232a4b
Ahmed Reda <ahmedrf.dev@gmail.com>
2025-08-25 01:44:10 +0300

    Add unique ID generation retries for URL shortening and improve error handling

--- src/controllers/url.ts
+++ src/controllers/url.ts
@@ -7,6 +7,7 @@ import AppError from "../utils/appError.js";
 
 const BASE_URL: string = "http://localhost:3000/";
 const URL_ID_LENGTH: number = 4;
+const URL_ID_RETRIES: number = 50;
 
 interface shortUrlRequestBody {
   url: string;
@@ -17,25 +18,31 @@ export const shortUrl = async (req: Request, res: Response) => {
 
   try {
     new URL(originalUrl);
-  } catch (err: any) {
-    return new AppError(err.message, 422);
+  } catch {
+    throw new AppError("Invalid url format", 422);
   }
 
-  let shortedUrlId: string;
-  while (true) {
+  let shortedUrlId: string = "";
+  let idFound: boolean = false;
+  for (let i = 0; i < URL_ID_RETRIES; i++) {
     shortedUrlId = randomBytes(URL_ID_LENGTH).toString("base64url");
 
     const oldUrlId: object | null = await Url.findOne({ shortedUrlId });
     if (!oldUrlId) {
       await Url.create({ originalUrl, shortedUrlId });
+      idFound = true;
       break;
     }
+  }
+
+  if (!idFound) {
+    throw new Error("Failed to generate unique short url");
   }
 
   const shortedUrl: string = BASE_URL + shortedUrlId;
 
   res.jsend.success(
-    { message: "Shorted url created successfully", shortedUrl },
+    { message: "Short URL created successfully", shortedUrl },
     201
   );
 };
