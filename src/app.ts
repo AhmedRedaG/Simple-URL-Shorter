bbf0d2a3261e7f889393569d11758feb727ed626
Ahmed Reda <ahmedrf.dev@gmail.com>
2025-08-24 22:45:21 +0300

    Add CORS and JSend middleware

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express from "express";
+import jsendMiddleware from "jsend-middleware";
+import cors from "cors";
 
 import urlRouter from "./routers/url.js";
 
 const app = express();
 
+app.use(cors());
+app.use(jsendMiddleware());
 app.use(express.json());
 
 app.use("/", urlRouter);
